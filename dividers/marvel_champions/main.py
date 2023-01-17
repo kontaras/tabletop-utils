36c81f248468e88750cd46f8ab23c5e1bc28988d
Konstantin Tarashchanskiy <2186793+kontaras@users.noreply.github.com>
2023-01-16 20:23:48 -0500

    Added Kree Fanatic set

--- dividers/marvel_champions/main.py
+++ dividers/marvel_champions/main.py
@@ -1,11 +1,12 @@
 from renderer import render
 from sets.campaign import *
 from sets.hero import *
+from sets.pnp import *
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    releases = [sinister_motives, scarlet_witch, gamora]
+    releases = [sinister_motives, scarlet_witch, gamora, ronan]
     sets = []
     for release in releases:
         sets += release.get_heroes()
---
+++ dividers/marvel_champions/sets/pnp.py
@@ -0,0 +1,13 @@
+from model import Release, Modular
+
+ronan=Release(
+    modulars=[Modular("Kree Fanatic",
+        cards={
+            "Ronan the Accuser": 1,
+            "Judge, Jury, Executioner": 1,
+            "The Accused": 1,
+            "Bring the Hammer Down": 2,
+            "\"You Dare Oppose Me?\"": 2
+        }
+    )]
+)
